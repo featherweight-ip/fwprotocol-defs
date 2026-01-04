dd6624093587e08005af3e8278f33e247fa0eefc
Matthew Ballance <matt.ballance@gmail.com>
2026-01-04 21:57:25 +0000

    XX
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/org/featherweight_ip/protocol/core/wishbone.py
+++ src/org/featherweight_ip/protocol/core/wishbone.py
@@ -10,6 +10,19 @@ class WishboneInitiator(zdc.Bundle):
     dat_r : zdc.bitv = zdc.input(width=lambda s:s.DATA_WIDTH)
     cyc : zdc.bit = zdc.output()
     err : zdc.bit = zdc.input()
-    sel : zdc.bitv = zdc.input(width=lambda s:int(s.DATA_WIDTH/8))
+    sel : zdc.bitv = zdc.output(width=lambda s:int(s.DATA_WIDTH/8))
     ack : zdc.bit = zdc.input()
     we : zdc.bit = zdc.output()
+
+@zdc.dataclass
+class WishboneTarget(zdc.Bundle):
+    DATA_WIDTH : zdc.u32 = zdc.const(default=32)
+    ADDR_WIDTH : zdc.u32 = zdc.const(default=32)
+    adr : zdc.bitv = zdc.input(width=lambda s:s.ADDR_WIDTH)
+    dat_w : zdc.bitv = zdc.input(width=lambda s:s.DATA_WIDTH)
+    dat_r : zdc.bitv = zdc.output(width=lambda s:s.DATA_WIDTH)
+    cyc : zdc.bit = zdc.input()
+    err : zdc.bit = zdc.output()
+    sel : zdc.bitv = zdc.input(width=lambda s:int(s.DATA_WIDTH/8))
+    ack : zdc.bit = zdc.output()
+    we : zdc.bit = zdc.input()
